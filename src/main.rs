2cdc364c0555d3903af33886dd4c731a085a46f1
gilflorida2023 <gilflorida2023@gmail.com>
2024-12-19 11:48:52 -0600

    modified the code to properly rely on golden ratio.

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,20 @@
 use nannou::prelude::*;
+
 struct Model {
+    length: f32,
+    a: Point2,
+    b: Point2,
+    center: Point2,
+    phi: f32,
+    phi_sqrt: f32,
+    phi_point: Point2,
+    thickness: f32,
+    smallcircle_radius: f32,
 }
 
+
 fn main() {
-    nannou::app(model).update(update).run();
+    nannou::app(model).view(view).run();
 }
 
 fn model(app: &App) -> Model {
@@ -12,82 +23,140 @@ fn model(app: &App) -> Model {
         .view(view)
         .build()
         .unwrap();
+    let length = 200.0;
+    let phi = (1.0 + f32::sqrt(5.0)) / 2.0;
+    let phi_sqrt = f32::sqrt(phi);
+    
     Model {
+        length,
+        a: pt2(-length, 0.0),
+        b: pt2(length, 0.0),
+        center: pt2(0.0, 0.0),
+        phi,
+        phi_sqrt,
+        phi_point: pt2(0.0, phi_sqrt * length),
+        thickness: 3.8,
+        smallcircle_radius: length * phi_sqrt - length,
     }
 }
 
-fn update(_app: &App, _model: &mut Model, _update: Update) {
-}
+/*fn update(_app: &App, _model: &mut Model, _update: Update) {
+}*/
 
-fn view(app: &App, _model: &Model, frame: Frame) {
+fn view(app: &App, model: &Model, frame: Frame) {
     let draw = app.draw();
     draw.background().color(WHITE);
-    let length = 200.0;
-    let a = pt2(-length,0.0);
-    let b = pt2(length,0.0);
-    let center = pt2(0.0,0.0);
-    let phi = (1.0 + f32::sqrt(5.0)) / 2.0; 
-    let phi_sqrt = f32::sqrt(phi);
-    let phi_point = pt2(0.0,phi_sqrt*length);
-    draw.line()
-        .start(a)
-        .end(center)
-        .color(BLUE)
-        .stroke_weight(2.0);
 
     draw.line()
-        .start(center)
-        .end(b)
+        .start(model.a)
+        .end(model.b)
         .color(RED)
-        .stroke_weight(2.0);
-
-
-    draw.line()
-        .start(a)
-        .end(phi_point)
-        .color(BLUE)
-        .stroke_weight(2.0);
+        .stroke_weight(model.thickness);
 
     draw.line()
-        .start(phi_point)
-        .end(b)
+        .start(model.a)
+        .end(model.phi_point)
         .color(RED)
-        .stroke_weight(2.0);
-        
+        .stroke_weight(model.thickness);
+
+    draw.arrow()
+    .weight(3.0) // Set the line width
+    .color(BLUE) 
+    .points(model.a, model.phi_point); 
+    draw.arrow()
+    .weight(3.0) // Set the line width
+    .color(BLUE) 
+    .points( model.phi_point,model.a); 
+
+    // Draw the label with mathematical symbols
+    let label_position = Point2::new(
+        (model.a.x + model.phi_point.x) / 2.0, 
+        (model.a.y + model.phi_point.y) / 2.0 
+    );
+
+    draw.text(&format!("φ")) // Directly draw the text
+        .xy(label_position)
+        .color(BLUE)
+        .font_size(72);
+
+    draw.line()
+        .start(model.phi_point)
+        .end(model.b)
+        .color(RED)
+        .stroke_weight(model.thickness);
+
+    draw.arrow()
+    .weight(3.0) // Set the line width
+    .color(BLUE) 
+    .points(model.center, model.phi_point); 
+    draw.arrow()
+    .weight(3.0) // Set the line width
+    .color(BLUE) 
+    .points( model.phi_point,model.center); 
+
+    // Draw the label with mathematical symbols
+    let label_position = Point2::new(
+        (model.center.x + model.phi_point.x) / 2.0, 
+        (model.center.y + model.phi_point.y) / 2.0 
+    );
+    draw.text(&format!("√φ")) // Directly draw the text
+        .xy(label_position)
+        .color(BLUE)
+        .font_size(72);
+    
+    draw.arrow()
+    .weight(3.0) // Set the line width
+    .color(BLUE) 
+    .points(model.center, model.a); 
+    draw.arrow()
+    .weight(3.0) // Set the line width
+    .color(BLUE) 
+    .points( model.a,model.center); 
+
+    // Draw the label with mathematical symbols
+    let label_position = Point2::new(
+        (model.center.x + model.a.x) / 2.0, 
+        (model.center.y + model.a.y) / 2.0 
+    );
+    draw.text(&format!("1")) // Directly draw the text
+        .xy(label_position)
+        .color(BLUE)
+        .font_size(72);
+
     draw.ellipse()
-        .xy(center)
-        .radius(length)
+        .xy(model.center)
+        .radius(model.length)
         .no_fill()
         .stroke(RED)
-        .stroke_weight(2.0);
+        .stroke_weight(model.thickness);
 
     draw.ellipse()
-        .xy(center)
-        .radius(length*phi_sqrt)
+        .xy(model.center)
+        .radius(model.length*model.phi_sqrt)
         .no_fill()
-        .stroke(CYAN)
-        .stroke_weight(2.0);
+        .stroke(RED)
+        .stroke_weight(model.thickness);
 
     draw.rect()
-        .xy(center)
-        .w_h(length*2.0, length*2.0)
+        .xy(model.center)
+        .w_h(model.length*2.0, model.length*2.0)
         .no_fill()
         .stroke(BLUE)
-        .stroke_weight(2.0);
+        .stroke_weight(model.thickness);
 
     draw.ellipse()
-        .xy(phi_point)
-        .radius(length*phi_sqrt-length)
+        .xy(model.phi_point)
+        .radius(model.smallcircle_radius)
         .no_fill()
-        .stroke(CYAN)
-        .stroke_weight(2.0);
+        .stroke(ORANGE)
+        .stroke_weight(model.thickness);
     
     draw.rect()
-        .xy(phi_point)
-        .w_h((length*phi_sqrt-length)*2.0,(length*phi_sqrt-length)*2.0)
+        .xy(model.phi_point)
+        .w_h((model.smallcircle_radius)*2.0,(model.smallcircle_radius)*2.0)
         .no_fill()
-        .stroke(CYAN)
-        .stroke_weight(2.0);
+        .stroke(ORANGE)
+        .stroke_weight(model.thickness);
 
     draw.to_frame(app, &frame).unwrap();
      // Save the frame when 'S' key is pressed
